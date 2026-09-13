7bae94f81b876a17a724c6597db576d9ed0ea81c
redwanweb353 <redwanweb353@gmail.com>
2026-09-13 15:54:36 +0600

    all file recheck

--- src/componants/Footer.tsx
+++ src/componants/Footer.tsx
@@ -3,17 +3,17 @@ const Footer = () => {
     return (
         <div className=' md:mt-25 py-6 md:pt-15 border-t border-gray-200'>
             <div className='max-w-7xl mx-auto p-4 md:p-0 text-[12px] '>
-                <div className='w-7xl mx-auto grid md:grid-cols-5 gap-5 py-4 md:py-0 text-[12px] pb-2 md:pb-8 border-b border-gray-200'>
-                    <div className='md:col-span-2 '>
+                <div className='max-w-7xl mx-auto grid md:grid-cols-5 gap-5 py-4 md:py-0 text-[12px] pb-2 md:pb-8 border-b border-gray-200'>
+                    <div className='md:col-span-2 md:w-full flex flex-col justify-center items-center'>
                         <img src={Logo} alt="Devstack" />
-                        <p className='my-4 text-[#64748B]'>Curated tools, technologies, and resources for developers building <br /> modern software.</p>
-                        <div className='flex mt-6 gap-6 font-bold text-[#475569]'>
+                        <p className='text-center my-4 text-[#64748B]'>Curated tools, technologies, and resources for developers building <br /> modern software.</p>
+                        <div className='flex my-6 gap-6 font-bold text-[#475569]'>
                             <span>GitHub</span>
                             <span> Twitter
                             </span><span>LinkedIn</span>
                         </div>
                     </div>
-                    <div className='md:col-span-3 flex justify-between list-none text-[#64748B]'>
+                    <div className=' hidden col-span-3 md:flex justify-between list-none text-[#64748B]'>
                         <div className='flex flex-col gap-2'>
                             <h2 className='font-bold text-[#0F172A] mb-2' >PRODUCT</h2>
                             <li>Home</li>
